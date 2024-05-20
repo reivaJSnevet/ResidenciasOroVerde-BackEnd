3f0e458ee96999e209b3727aa15ec3e7fd2a156e
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2024-05-20 00:27:08 -0600

    Update Comment model field name to 'date'

--- models/Comment.js
+++ models/Comment.js
@@ -24,7 +24,7 @@ const Comment = db.define("Comment", {
             },
         },
     },
-    fechaPublicacion: {
+    date: {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW,
         allowNull: false,      
