a6d612bc08ceb95fa89fcd35a0e7832a1d62fe3b
Rosi-07 <cuberoflores07@gmail.com>
2024-05-28 23:41:11 -0600

    Add new seeders in properties and categories

--- seed/categories.js
+++ seed/categories.js
@@ -1,88 +1,55 @@
 const categories = [
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40837",
-        name: "casa"
-    },
+
     {
         id: "8fcdc613-744b-449e-b23c-e8ef28b40838",
-        name: "apartamento"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40839",
-        name: "lote"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40840",
-        name: "local"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40841",
-        name: "bodega"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40842",
-        name: "oficina"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40843",
-        name: "finca"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40836",
-        name: "hacienda"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40844",
-        name: "edificio"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40845",
-        name: "otro"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40846",
-        name: "terreno"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40847",
-        name: "casa de playa"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40848",
-        name: "casa de montaña"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40849",
-        name: "casa de ciudad"
+        name: "Apartamento"
     },
     {
         id: "8fcdc613-744b-449e-b23c-e8ef28b40850",
-        name: "casa de campo"
+        name: "Casa de campo"
     },
     {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40851",
-        name: "chalet"
+        id: "8fcdc613-744b-449e-b23c-e8ef28b40849",
+        name: "Casa de ciudad"
     },
     {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40852",
-        name: "quinta"
+        id: "8fcdc613-744b-449e-b23c-e8ef28b40848",
+        name: "Casa de montaña"
     },
     {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40853",
-        name: "rancho"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40854",
-        name: "villa"
-    },
-    {
-        id: "8fcdc613-744b-449e-b23c-e8ef28b40855",
-        name: "bungalow"
+        id: "8fcdc613-744b-449e-b23c-e8ef28b40847",
+        name: "Casa de playa"
     },
     {
         id: "8fcdc613-744b-449e-b23c-e8ef28b40856",
-        name: "casa de madera"
+        name: "Casa de madera"
     },
+
+    {
+        id: "8fcdc613-744b-449e-b23c-e8ef28b40855",
+        name: "Condominio"
+    },
+    {
+        id: "8fcdc613-744b-449e-b23c-e8ef28b40844",
+        name: "Edificio"
+    },
+    {
+        id: "8fcdc613-744b-449e-b23c-e8ef28b40843",
+        name: "Finca"
+    },
+    {
+        id: "8fcdc613-744b-449e-b23c-e8ef28b40836",
+        name: "Hacienda"
+    },
+
+    {
+        id: "8fcdc613-744b-449e-b23c-e8ef28b40853",
+        name: "Rancho"
+    },
+    {
+        id: "8fcdc613-744b-449e-b23c-e8ef28b40854",
+        name: "Villa"
+    }
 
 ];
 
--- seed/seeder.js
+++ seed/seeder.js
@@ -14,12 +14,25 @@ const seeder = async () => {
         await Comment.bulkCreate(comments);
         await Rating.bulkCreate(ratings);
 
-        await newProperties[0].addCategories(newCategories[15]);
-        await newProperties[1].addCategories(newCategories[2]);
-        await newProperties[2].addCategories(newCategories[12]);
-
-
-        
+        await newProperties[0].addCategories(newCategories[1]);
+        await newProperties[1].addCategories(newCategories[0]);
+        await newProperties[2].addCategories(newCategories[4]);
+        await newProperties[3].addCategories(newCategories[2]);
+        await newProperties[4].addCategories(newCategories[2]);
+        await newProperties[5].addCategories(newCategories[12]);
+        await newProperties[6].addCategories(newCategories[9]);
+        await newProperties[7].addCategories(newCategories[5]);
+        await newProperties[8].addCategories(newCategories[2]);
+        await newProperties[9].addCategories(newCategories[2]);
+        await newProperties[10].addCategories(newCategories[7]);
+        await newProperties[11].addCategories(newCategories[12]);
+        await newProperties[12].addCategories(newCategories[3]);
+        await newProperties[13].addCategories(newCategories[2]);
+        await newProperties[14].addCategories(newCategories[12]);
+        await newProperties[15].addCategories(newCategories[7]);
+        await newProperties[16].addCategories(newCategories[4]);
+        await newProperties[17].addCategories(newCategories[4]);
+     
 
         console.log(
             "\x1b[32m%s\x1b[0m",
